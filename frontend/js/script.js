fee65ddbafc48977d31f061e2743b8d264507c1b
jonatas <jonatasprado445@gmail.com>
2023-11-28 17:37:13 -0300

    ajustes

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -86,7 +86,7 @@ const handleLogin = (event) => {
     login.style.display = "none" 
     chat.style.display= "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-2liv.onrender.com")
     websocket.onmessage = processMessage
 
 }
